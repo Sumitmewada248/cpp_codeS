84a016283bc9b12765e67b2e1dcba9f98a675df9
Sumitmewada248 <sumitmewa@gmail.com>
2024-10-05 12:25:23 +0530

    2D array

--- 2d_array/05oct.cpp
+++ 2d_array/05oct.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main(){
+/*
+int arr[2][3];
+cout<<"enter 6 value \n";
+for(int i=0;i<2;i++){
+    for(int j=0;j<3;j++){
+  cin>>arr[i][j];
+    }
+}
 
-int arr[2][3]={2,5,8,6,7,2};
+cout<<"\n stored data is displayed \n";
 
 for(int i=0;i<2;i++){
     for(int j=0;j<3;j++){
@@ -12,6 +20,31 @@ for(int i=0;i<2;i++){
 
 cout<<endl;
 }
+*/
+
+int arr[3][3];
+cout<<"enter 9 value \n";
+for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+  cin>>arr[i][j];
+    }
+}
+
+cout<<"\n even values only\n";
+
+for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+            if(arr[i][j]%2==0){
+     cout<<arr[i][j]<<"\t";
+            }
+   // else{
+    //    cout<<"0"<<"\t";
+   // }
+    }
+
+cout<<endl;
+}
+
 
 
 }
