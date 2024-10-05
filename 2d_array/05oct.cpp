3e2758a7c86a75a53529f04f67abf2b389b407d0
Sumitmewada248 <sumitmewa@gmail.com>
2024-10-05 12:01:37 +0530

    2D array

---
+++ 2d_array/05oct.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int arr[2][3]={2,5,8,6,7,2};
+
+for(int i=0;i<2;i++){
+    for(int j=0;j<3;j++){
+     cout<<arr[i][j]<<"\t";
+    }
+
+cout<<endl;
+}
+
+
+}
