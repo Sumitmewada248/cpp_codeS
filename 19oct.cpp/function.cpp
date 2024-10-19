0334151b77599c9cc0ee92bbe478991a14f57d63
Sumitmewada248 <sumitmewa@gmail.com>
2024-10-19 12:39:23 +0530

    function

---
+++ 19oct.cpp/function.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+  void show(){
+  cout<<"weldone \n";
+  }
+
+  void sum(int a,int b){
+  cout<<a+b;
+  }
+
+
+int main(){
+
+
+show();
+show();
+sum(15,25);
+}
+
+
+
+
+
+
