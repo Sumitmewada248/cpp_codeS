cd4d225d41b0fe4c54aec08256b018bd993ef12c
Sumitmewada248 <sumitmewa@gmail.com>
2024-11-26 20:19:50 +0530

    basic constructor

---
+++ class/constructor.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//Default Constructor
+
+//Syntax
+
+//public:classname()
+//{
+//statement;
+//}
+
+//eXAMPLE
+class stu
+{
+public: stu()
+
+
+{
+cout<<"alllow";
+}
+};
+
+int main(){
+stu r;
+}
