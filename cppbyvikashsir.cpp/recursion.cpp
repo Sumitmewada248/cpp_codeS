8f4a7d3bb0cfdbaf63b3692dc464188f4ff36556
Sumit mewada <sumitmewa@gmail.com>
2025-01-01 09:51:19 +0530

    Update recursion.cpp

--- cppbyvikashsir.cpp/recursion.cpp
+++ cppbyvikashsir.cpp/recursion.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 using namespace std;
-
-
 int  cybrom (int a)
 {
     if(a<1){
@@ -9,14 +7,10 @@ int  cybrom (int a)
         return a;
     }
     cout<<a<<"\n";// Head recursion
-
     cybrom(a-1);
        cout<<a<<"\n";// Tail recursion
 }
 int main(){
 
         cybrom(5);
-
-
-
 }
